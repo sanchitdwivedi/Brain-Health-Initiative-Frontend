c9b48e778d9618b60ed4052cc7c844ab97afdbfb
Khushal Abrol <khushalabrol14@gmail.com>
2022-02-24 17:16:18 +0530

    Resolved: Register patient functionality (By Brute Force, need to be optimized)

--- src/app/doctor/create/create.component.ts
+++ src/app/doctor/create/create.component.ts
@@ -23,39 +23,58 @@ export class CreateComponent implements OnInit {
   gender = '';
   education = '';
   dob = '';
+  patient_detail: any;
 
 
 
 
-  constructor(private doctorService: DoctorService, 
-              private patientDetailService: PatientDetailService) { }
+  constructor(private doctorService: DoctorService,
+    private patientDetailService: PatientDetailService) { }
 
   ngOnInit(): void {
     this.created = '';
   }
 
-  create(createPatientForm: NgForm){
-    console.log(createPatientForm.value);
+  create(createPatientForm: NgForm) {
+    console.log("createPatient Called")
+    var response = this.patient_detail;
+    console.log(response)
+    console.log("Create Patient Values: ", createPatientForm.value);
+    
+    createPatientForm.value.first_name = response.first_name;
+    createPatientForm.value.last_name = response.last_name;
+    createPatientForm.value.address_line_1 = response.address_line_1;
+    createPatientForm.value.address_line_2 = response.address_line_2;
+    createPatientForm.value.district = response.district;
+    createPatientForm.value.mobile_no = response.mobile_no;
+    createPatientForm.value.pin_code = response.pin_code;
+    createPatientForm.value.socioeconomic_status = response.socioeconomic_status;
+    createPatientForm.value.state = response.state;
+    createPatientForm.value.gender = response.gender;
+    createPatientForm.value.education = response.education;
+    createPatientForm.value.dob = response.dob;
+
     this.doctorService.createPatient(createPatientForm.value).subscribe({
       next: (response: any) => {
-        // console.log(response);
+        console.log(response);
         this.created = '1';
       },
-      error: (error) => {
+      error: (error: any) => {
         console.log(error);
         this.created = '0';
       }
     });
   }
 
-  search(){
+  search() {
     console.log(this.abhaId);
     this.patientDetailService.getDetail(this.abhaId).subscribe({
       next: (response: any) => {
-        if(response===null){
+        if (response === null) {
           alert(`ABHA ID: ${this.abhaId} is Invalid!`);
-        }else{
+        } else {
           // console.log("API Response: ",response);
+          this.patient_detail = response;
           this.first_name = response.first_name;
           this.last_name = response.last_name;
           this.address_line_1 = response.address_line_1;
@@ -66,17 +85,17 @@ export class CreateComponent implements OnInit {
           this.socioeconomic_status = response.socioeconomic_status;
           this.state = response.state;
           this.gender = response.gender;
-          this.education = response.education; 
+          this.education = response.education;
           this.dob = response.dob;
         }
       },
       error: (error) => {
         console.log(error);
-        
+
       }
-     })
+    })
   }
 
-  
+
 
 }
