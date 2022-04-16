28fc5173a132db9ef881a5a55e1256416810b7c5
Khushal Abrol <khushalabrol14@gmail.com>
2022-04-16 22:02:38 +0530

    Added: Admin add, update, delete to admin table

--- src/app/admin/forms/admin-form/admin-form.component.ts
+++ src/app/admin/forms/admin-form/admin-form.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Admin } from 'src/app/interfaces/Admin';
+import { Role } from 'src/app/interfaces/Role';
+import { User } from 'src/app/interfaces/User';
 import { AdminService } from 'src/app/_services/admin.service';
 
 @Component({
@@ -9,110 +13,39 @@ import { AdminService } from 'src/app/_services/admin.service';
 })
 export class AdminFormComponent implements OnInit {
 
+  update: boolean = false;
+  add: boolean = false;
   @Input() tableName: string;
-  addDoctorForm: FormGroup;
-  addRoleForm: FormGroup;
-  addLevelForm: FormGroup;
-  addHospitalForm: FormGroup;
-  levels: any = [];
+  adminDetail: any;
   roles: any = [];
-  hospitals: any = [];
-  readonly: boolean;
+  role: Role = ({} as any) as Role;
+  admin: Admin = ({} as any) as Admin;
+  user: User = ({} as any) as User;
+
 
   constructor(
+    @Inject(MAT_DIALOG_DATA) private operationAndDate: any,
     private fb: FormBuilder,
     private adminService: AdminService
   ) {
-
-    this.addDoctorForm = this.fb.group({
-      doctor: this.fb.group({
-        userId: [''],
-        password: [''],
-        confirmPassword: [''],
-        role: this.fb.group({
-          roleName: ['']
-        })
-      }),
-      firstName: [''],
-      lastName: [''],
-      gender: [''],
-      district: [''],
-      state: [''],
-      city: [''],
-      pincode: [''],
-      mobileNo: [''],
-      email: [''],
-      hospital: this.fb.group({
-        hospitalId: ['']
-      })
-    });
-
-    this.addRoleForm = this.fb.group({
-      roleName: [''],
-      roleDescription: ['']
-    });
-
-    this.addLevelForm = this.fb.group({
-      levelName: [''],
-      levelDescription: ['']
-    });
-
-    this.addHospitalForm = this.fb.group({
-      hospitalName: [''],
-      pincode: [''],
-      city: [''],
-      state: [''],
-      district: [''],
-      level: this.fb.group({
-        levelName: ['']
-      }),
-    });
-
+    console.log("operationAndDate.element: ", operationAndDate.element);
+    if (operationAndDate.operation === 'add') {
+      this.add = true;
+    } else if (operationAndDate.operation === 'update') {
+      this.update = true;
+    }
+    this.adminDetail = operationAndDate.element;
   }
 
   ngOnInit(): void {
-    this.getLevels();
     this.getRoles();
-    this.getHospitals();
-    console.log(this.levels);
   }
 
-  public get doctor(): FormGroup {
-    return this.addDoctorForm.get('doctor') as FormGroup;
-  }
-
-  public get role(): FormGroup {
-    return this.doctor.get('role') as FormGroup;
-  }
-
-  public get hospital(): FormGroup {
-    return this.addDoctorForm.get('hospital') as FormGroup;
-  }
-
-  public get levelForm(): FormGroup {
-    return this.addHospitalForm.get('level') as FormGroup;
-  }
-
-
-
-  //Needed: Error Here
-  getLevels() {
-    this.levels = this.adminService.getLevels().subscribe({
-      next: (response: any) => {
-        this.levels = response;
-        console.log("Level: ", response);
-      },
-      error: (error: any) => {
-        console.log(error);
-      }
-    });
-  }
-
+  
   getRoles() {
     this.roles = this.adminService.getRoles().subscribe({
       next: (response: any) => {
         this.roles = response;
-        console.log("roles", this.roles)
       },
       error: (error: any) => {
         console.log(error);
@@ -120,36 +53,8 @@ export class AdminFormComponent implements OnInit {
     });
   }
 
-  getHospitals() {
-    this.hospitals = this.adminService.getHospitals().subscribe({
-      next: (response: any) => {
-        this.hospitals = response;
-        console.log(this.hospitals);
-      },
-      error: (error: any) => {
-        console.log(error);
-      }
-    })
-  }
-
-  addDoctor() {
-    console.log(this.addDoctorForm.value);
-    if (this.doctor.value.password === this.doctor.value.confirmPassword) {
-      this.adminService.addDoctor(this.addDoctorForm).subscribe({
-        next: (response: any) => {
-          console.log(response);
-        },
-        error: (error: any) => {
-          console.log(error);
-        }
-      });
-    } else {
-      alert('Passwords do not match!');
-    }
-  }
-
-  addRole() {
-    this.adminService.addRole(this.addRoleForm).subscribe({
+  addAdmin(adminDetails:any) {
+    this.adminService.addAdmin(adminDetails.value).subscribe({
       next: (response: any) => {
         console.log(response);
       },
@@ -157,11 +62,10 @@ export class AdminFormComponent implements OnInit {
         console.log(error);
       }
     });
-    this.getRoles();
   }
 
-  addLevel() {
-    this.adminService.addLevel(this.addLevelForm).subscribe({
+  updateAdmin(adminDetails:any) {
+    this.adminService.updateAdmin(adminDetails.value).subscribe({
       next: (response: any) => {
         console.log(response);
       },
@@ -169,19 +73,6 @@ export class AdminFormComponent implements OnInit {
         console.log(error);
       }
     });
-    this.getLevels();
-  }
-
-  addHospital() {
-    this.adminService.addHospital(this.addHospitalForm).subscribe({
-      next: (response: any) => {
-        console.log(response);
-      },
-      error: (error: any) => {
-        console.log(error);
-      }
-    });
-    this.getHospitals();
   }
 
 }
