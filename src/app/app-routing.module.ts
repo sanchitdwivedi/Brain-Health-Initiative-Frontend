a03f401f8105806776a861ba877b3e51aed5fba7
unknown <sanchitdwivedi123@gmail.com>
2022-04-20 01:03:36 +0530

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
   { path: 'update-password/:id', component: UpdatePasswordComponent },
   { path: '**', component: LoginComponent }
 ];
-
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
